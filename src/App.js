09cca8f7c0cdc90751687cae1fbd3e22b4ab3627
MacBookPro <stephenjcwalton@gmail.com>
2020-10-30 17:10:24 +0000

    client side files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+//add my onsubmit handler to sendemail
 
 function App() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (e.target.id === "name") {
+      setName(e.target.value)
+    }
+    else {
+      setEmail(e.target.value)
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const dataToSubmit = {
+      name,
+      email
+    }
+
+    axios.post("/api/sendMail", dataToSubmit)
+
+  }
+
+  // FE
+  // http://localhost:3000
+
+  // BE
+  // http://localhost:5000
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        EMAIL TESTING
+        <form onSubmit={handleSubmit}>
+          <input id="name" placeholder="Name" value={name} onChange={handleClick} />
+          <input id="email" placeholder="Email" value={email} onChange={handleClick} /><br />
+          <button onClick={handleSubmit}>Send Email</button>
+        </form>
+
       </header>
+
     </div>
   );
 }
+
 
 export default App;
